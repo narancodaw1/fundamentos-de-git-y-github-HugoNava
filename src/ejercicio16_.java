fa287f193b88afdd8fbee796c244722c93d969d3
alumno_mañana <alumno_maana@PC22>
2021-11-10 12:37:21 +0100

    primer comit

---
+++ src/ejercicio16_.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ejercicio16_ {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner (System.in);
+	int nunmero = sc.nextInt();
+	boolean esPrimo = true;
+	int contador = 2;
+	While(esPrimo==true && contador<numero)  {
+		if(numero%contador == 0)  {
+			 esPrimo==false;
+		}else {
+			contador++
+			
+		}
+	
+	}
+	
+	if(esPrimo==true) {
+		
+		
+	}else {
+		System,out.println("El número introducido es primo");
+		
+	    }else {
+	    	
+	    
+	    	   System.out.println("El numero introducido NO es primo");    	   
+	   
+	   {
+	}
+	   
+	   
+}
